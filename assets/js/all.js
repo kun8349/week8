898926ab50346ffee5fc7f875bf6b164e531c0d5
kun8349 <108168475+kun8349@users.noreply.github.com>
2022-09-05 21:50:38 +0800

    Update 2022-09-05T13:50:36.068Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,29 @@
 "use strict";
 
+//手機板
 var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true
-  }
-});
-var swiper_lg = new swiper_lg(".mySwiper_lg", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
+  },
+  autoplay: {
+    delay: 2500,
+    stopOnLastSlide: false,
+    disableOnInteraction: false
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      centeredSlides: true
+    }
   }
 });
 $(".swiper-pagination-bullet-active").on('click', function (e) {
