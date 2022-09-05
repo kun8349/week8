1fa6188f1734a1f4dd9bd3bbcc06d2afecda5cc2
kun8349 <108168475+kun8349@users.noreply.github.com>
2022-09-05 15:33:35 +0800

    9/5upadte

--- Desktop/week8/app/assets/js/all.js
+++ Desktop/week8/app/assets/js/all.js
@@ -1,3 +1,17 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper_lg = new swiper_lg(".mySwiper_lg", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(".swiper-pagination-bullet-active").on('click',function (e) {
+  e.preventDefault();
+  $(this).toggleClass(".swiper-pagination-bullet-active");
 });
